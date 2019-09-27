9cdb51f6f0110c153e85054614cdb0aa5dffc581
neesmusuns <irl3@hi.is>
2019-09-27 15:04:31 +0000

    initial commit

---
+++ src/main/java/is/hi/hbv501/pirategame/pirategame/PirategameApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501.pirategame.pirategame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PirategameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PirategameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/is/hi/hbv501/pirategame/pirategame/PirategameApplicationTests.java
@@ -0,0 +1,16 @@
+package is.hi.hbv501.pirategame.pirategame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PirategameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
